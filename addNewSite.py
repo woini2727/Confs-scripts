1a1ef419f2aa894e7624fbea24ed346338d48eba
ralfonso-rb <rosa.alfonso@redb.ee>
2024-08-08 15:11:42 -0300

    agregando logica para modificar una lista de sites

--- addNewSite.py
+++ addNewSite.py
@@ -5,6 +5,16 @@ import re
 import json
 import sys
 
+def siteFileToList(siteFile):
+    file = open(siteFile, 'r', encoding="utf8")
+    lines = file.readlines()
+    siteList = []
+    for line in lines:
+        site = line.strip().rstrip(',')
+        if site:  
+            siteList.append(site)
+    return siteList
+
 directory = sys.argv[1]
 nuevoMedioPago = sys.argv[2]
 nuevoProtocolo = sys.argv[3]
@@ -15,7 +25,9 @@ protocolLine = 'idsprotocolos =>'
 
 siteline = ' =>' 
 siteList = siteFileToList(listaSiteDirectory)
-
+print("lista de sitios: " + str(siteList))
+findSite = False
+changeMP= False
 for x in os.listdir(directory):
     if x.endswith(".conf"):
         file = open(directory + '/' + x, 'r', encoding="utf8")
@@ -23,34 +35,31 @@ for x in os.listdir(directory):
         lineCounter = 0
         for line in lines:
             for site in siteList:
-                siteL = site + siteline
+                siteL = '"' + site + '"' + siteline
                 if siteL in line:
-                    if medioPagoLine in line:
-                        pagos = re.findall(r'\d+', line)
-                        if nuevoMedioPago in pagos:
-                            break
-                        pagos.append(nuevoMedioPago)
-                        pagosListToStr = "".join([elem for elem in json.dumps(pagos)])
-                        lines[lineCounter] = '\t' * 2 + medioPagoLine + pagosListToStr + ',' + '\n'
-                
-                    if protocolLine in line:
-                        protocolos = re.findall(r'\d+', line)
-                        if nuevoProtocolo in pagos:
-                            break
-                        protocolos.append(nuevoProtocolo)
-                        listToStr = ' '.join([str(elem) for elem in protocolos])
-                        protocoloListToStr = "".join([elem for elem in json.dumps(protocolos)])
-                        lines[lineCounter] = '\t' * 2 + medioPagoLine + protocoloListToStr + ',' + '\n'
-                    lineCounter += 1
+                    print("se encontro el sitio: " + siteL)
+                    findSite = True
+                    continue
+                if medioPagoLine in line and findSite == True:
+                    pagos = re.findall(r'\d+', line)
+                    if nuevoMedioPago in pagos:
+                        break
+                    pagos.append(nuevoMedioPago)
+                    pagosListToStr = "".join([elem for elem in json.dumps(pagos)])
+                    lines[lineCounter] = '\t' * 2 + medioPagoLine + pagosListToStr + ',' + '\n'
+                    changeMP = True
+                    continue
+                if protocolLine in line and changeMP == True:
+                    protocolos = re.findall(r'\d+', line)
+                    protocolos.append(nuevoProtocolo)
+                    listToStr = ' '.join([str(elem) for elem in protocolos])
+                    protocoloListToStr = "".join([elem for elem in json.dumps(protocolos)])
+                    lines[lineCounter] = '\t' * 2 + protocolLine + protocoloListToStr + ',' + '\n'
+                    changeMP = False
+                    findSite = False
+            lineCounter += 1
         newfile = open(directory + '/' + x, 'w', encoding="utf8")
         newfile.writelines(lines)
 
 
-def siteFileToList(siteFile):
-    file = open(siteFile, 'r', encoding="utf8")
-    lines = file.readlines()
-    siteList = []
-    for line in lines:
-        if siteline in line:
-            siteList.append(line.split(' => ')[0])
-    return None
+
